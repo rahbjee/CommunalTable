6635336c6ba94d6da8eeb276a0e30fa3355a387a
Rahul Bonnerjee <Rahul.Bonnerjee@gmail.com>
2019-03-19 23:20:32 -0400

    Initial Commit

---
+++ CommunalTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CommunalTable
+//
+//  Created by Rahul Bonnerjee on 3/19/19.
+//  Copyright © 2019 Rahbjee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
